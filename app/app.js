c8c9e097220ac644a7b4f0aa7e3fd67a082374b9
Tomas <katysovas@gmail.com>
2015-10-08 10:41:54 -0700

    cleaning

--- app/app.js
+++ app/app.js
@@ -1,3 +1,6 @@
+var BASE_URL = 'https://api.mongolab.com/api/1/databases/pagedb/collections';
+var API_KEY = 'OtIKk5xORSRSn6BatrDeGAqNcfnMLW70';
+
 var App = App || {};
 var Models = Models || {};
 
--- app/config.js
+++
@@ -1,3 +0,0 @@
-var BASE_URL = 'https://api.mongolab.com/api/1/databases/pagedb/collections';
-var API_KEY = 'OtIKk5xORSRSn6BatrDeGAqNcfnMLW70';
-
